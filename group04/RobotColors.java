dc8610f26a9db1827c1be0230d734157d46c08ee
yamaguchi04 <yamaguchi.unge04@gmail.com>
2018-12-07 15:53:41 +0900

    20181207 15:53 neo

---
+++ group04/RobotColors.java
@@ -0,0 +1,25 @@
+package group04;
+
+/**
+ * MyClass - a class by (your name here)
+ */
+
+import java.awt.*;
+
+
+/**
+ * RobotColors - A serializable class to send Colors to teammates.
+ *
+ * @author Mathew A. Nelson (original)
+ * @author Flemming N. Larsen (contributor)
+ */
+public class RobotColors implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	public Color bodyColor;
+	public Color gunColor;
+	public Color radarColor;
+	public Color scanColor;
+	public Color bulletColor;
+}
